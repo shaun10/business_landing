7d48e66b216e581d2c951718b96f42da6cbbcc1c
Shaun <eqsurf829@hotmail.com>
2015-10-15 23:01:02 -0700

    Adding checkout options and updates to page

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn project:app --log-file=-
+PUBLISHABLE_KEY=pk_test_6pRNASCoBOKtIshFeQd4XMUh SECRET_KEY=MadNessSecretKeyed10!!!233!21OK python project.py
--- project.py
+++ project.py
@@ -1,53 +1,44 @@
-from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
-from flask_mail import Mail, Message
-import datetime
-
-#from database_setup import Base, MessageItem
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import sessionmaker
+import os
+from flask import Flask, render_template, request
+import stripe
 
 
+stripe_keys = {
+    'secret_key': os.environ['SECRET_KEY'],
+    'publishable_key': os.environ['PUBLISHABLE_KEY']
+}
 
-# engine = create_engine('sqlite:///#.db')
-# Base.metadata.bind = engine
-
-# DBSession = sessionmaker(bind=engine)
-# session = DBSession()
-
-'''
-1. Mock up
-2. Routing can navigate to all the urls even if the pages are not yet completed
-3. Templates and Forms, and proper functionality, want to see the templates rendering in application
-4. CRUD Functionality, Create Read Update Delete
-5. API Endpoints, calls to browser are working alright.
-6. Styling and Message Flashing, CSS and Javascript incorporation
-Clear deliverable at the end of the iteration
-Main index updates
-'''
+stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
-mail = Mail(app)
-today = datetime.date
-
-#app.secret_key = '#'
 
 
-
-#First line is used for the main site url
-#useful for routing multiple urls to one place
-#specifying type
 @app.route('/')
-def mainPage():
-	return render_template('spa_temp.html')
+def index():
+    return render_template('index.html', key=stripe_keys['publishable_key'])
 
-	#flask allows us to render template instead of having to type line after line 
-	#of these output updates. Old way is below before there were rendered templates
+@app.route('/charge', methods=['POST'])
+def charge():
+    # Amount in cents
+    #Can use a get on the dropdown to choose the product to sell to the individual
+    #We just need to get the one used in the form then change the logic below to reflect the other product
+    amount = 500
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        card=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='Shampoo'
+    )
+
+    return render_template('charge.html', amount=amount)
 
 if __name__ == '__main__':
-	#Needs to be more secure for real project 
-	#Will reload the page without having to restart the server, useful
-	app.debug = True
-	#With framework response messages are simplified has a significant impact on code reduction
-	app.run(host = '0.0.0.0', port = 5000)
-	#app.run(host='0.0.0.0',debug=True)
-	#app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+	app.run(debug=False)
+
+app.secret_key = 'MadNessSecretKeyed10!!!233!21OK'
