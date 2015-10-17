7b1b8b91f0203f093a5a6f68d5b7ced1b63f4659
Shaun <eqsurf829@hotmail.com>
2015-10-17 09:27:31 -0700

    finally it works

--- project.py
+++ project.py
@@ -3,22 +3,22 @@ import stripe
 
 #web: PUBLISHABLE_KEY=pk_test_6pRNASCoBOKtIshFeQd4XMUh SECRET_KEY=MadNessSecretKeyed10!!!233!21OK python project.py
 PUBLISHABLE_KEY="pk_test_6pRNASCoBOKtIshFeQd4XMUh"
-SECRET_KEY="MadNessSecretKeyed10!!!233!21OK"
-app.secret_key = 'MadNessSecretKeyed10!!!233!21OK'
+SECRET_KEY="sk_test_BQokikJOvBiI2HlWgH4olfQ2"
+
 # stripe_keys = {
 #     'secret_key': os.environ['SECRET_KEY'],
 #     'publishable_key': os.environ['PUBLISHABLE_KEY']
 # }
 
 stripe_keys = {
-	'secret_key' = SECRET_KEY,
-	'publishable_key' = PUBLISHABLE_KEY
+	'secret_key': SECRET_KEY,
+	'publishable_key': PUBLISHABLE_KEY
 }
 
 stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
-
+app.secret_key = 'MadNessSecretKeyed10!!!233!21OK'
 
 @app.route('/')
 def index():
@@ -29,11 +29,11 @@ def charge():
     # Amount in cents
     #Can use a get on the dropdown to choose the product to sell to the individual
     #We just need to get the one used in the form then change the logic below to reflect the other product
-    amount = 22.00
+    amount = 2200
 
     customer = stripe.Customer.create(
-        #email='customer@example.com',
-        email =request.form['email']
+        email='customer@example.com',
+        #email =request.form['email'],
         card=request.form['stripeToken']
     )
 
@@ -47,5 +47,5 @@ def charge():
     return render_template('charge.html', amount=amount)
 
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(debug=True)
 
