903e1724206ef992ab75aa60aa51ec593f074001
Shaun <eqsurf829@hotmail.com>
2015-10-18 15:32:23 -0700

    fix app debug error

--- project.py
+++ project.py
@@ -92,6 +92,6 @@ def charge():
     return render_template('charge.html', amount=int(amount/100))
 
 if __name__ == '__main__':
-	app.run(debug=True)
-	#app.run(host='0.0.0.0', port=int(os.environ.get("PORT")))
+	#app.run(debug=True)
+	app.run(host='0.0.0.0', port=int(os.environ.get("PORT")))
 
